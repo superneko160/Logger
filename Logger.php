ec5d13c4ee4d87a645db9b806a942636d82ec9e3
sneko <supernekocat31@gmail.com>
2023-11-16 19:25:51 +0900

    add validation

--- Logger.php
+++ Logger.php
@@ -1,39 +1,59 @@
 <?php
 /**
- * ロガークラス（デバッグ用）
+ * ロガー（デバッグ用）
  */
 class Logger {
     /**
-     * ログファイルにデータの中身を書き出す
-     * @param string $logpath データを書き込むファイルのパス
-     * @param mixed $data ファイルに書き込むデータ
+     * ログファイルに変数or配列の中身を書き出す
+     * @param string $logpath データを書き込むファイルのパス（初期値：debug.log）
+     * @param mixed $data ファイルに書き込むデータ（初期値：The data to be verified is not set.）
      * @param string $mode ファイルの書き込みモード（初期値：w（上書きモード））
      * @return void
      */
-    public static function dumpLog(string $logpath, mixed $data, string $mode="w"): void {
+    public static function dumpLog(string $logpath="debug.log", mixed $data="The data to be verified is not set.", string $mode="w"): void {
         try {
             // ファイルパスの拡張子取得
             $ext = substr($logpath, strrpos($logpath, '.') + 1);
             if ($ext !== "txt" && $ext !== "log") {
                 throw new Exception("File extension should be txt or log.");
             }
+
             // ファイルを開く（存在しない場合は新規作成）
             $fileHandle = fopen($logpath, $mode);
-            // ファイルが開けなかった場合、エラーログに追記
+            // ファイルが開けなかった場合、エラーログに追記して処理お返し
             if ($fileHandle === false) {
                 error_log("Failed to open the file: {$logpath}");
                 return;
             }
+
+            // Null値の場合、なにも書き出されないのでNullの文字列に変換
+            if (is_null($data)) {
+                $data = "Null";
+            }
+
+            // ブール値の場合、1、0で書き出されるのでTrue、Falseの文字列に変換
+            if (is_bool($data)) {
+                if ($data === True) {
+                    $data = "True";
+                }
+                if ($data === False) {
+                    $data = "False";
+                }
+            }
+
             // 配列の場合：implode関数を使って文字列に変換
             if (is_array($data)) {
                 $data = implode(PHP_EOL, $data);
             }
+
             // オブジェクトの場合：var_exportを使って文字列に変換
             if (is_object($data)) {
                 $data = var_export($data, true);
             }
+
             // ファイルにデータを書き込み
             fwrite($fileHandle, $data . PHP_EOL);
+
             // ファイルを閉じる
             fclose($fileHandle);
         } catch (Exception $e) {
--- test/LoggerTest.php
+++ test/LoggerTest.php
@@ -3,7 +3,7 @@ use PHPUnit\Framework\TestCase;
 
 require_once 'Logger.php';
 
-// RUN: vendor/bin/phpunit test
+// TEST RUN: vendor/bin/phpunit test
 class LoggerTest extends TestCase
 {
     // テストファイルのパス
@@ -27,6 +27,14 @@ class LoggerTest extends TestCase
         }
     }
 
+    // 検証データが引数に設定されていない場合、初期値がファイルに書き込まれるかテスト
+    public function testDataNotSetWriteToLog()
+    {
+        Logger::dumpLog($this->testFilePath);
+        $this->assertFileExists($this->testFilePath);
+        $this->assertEquals("The data to be verified is not set.\r\n", file_get_contents($this->testFilePath));
+    }
+
     // 文字列が正しくファイルに書き込まれるかテスト
     public function testStringWriteToLog()
     {
@@ -36,6 +44,24 @@ class LoggerTest extends TestCase
         $this->assertEquals("test\r\n", file_get_contents($this->testFilePath));
     }
 
+    // 文字列（空）が正しくファイルに書き込まれるかテスト
+    public function testStringEmptyWriteToLog()
+    {
+        $data = "";
+        Logger::dumpLog($this->testFilePath, $data);
+        $this->assertFileExists($this->testFilePath);
+        $this->assertEquals("\r\n", file_get_contents($this->testFilePath));
+    }
+
+    // 文字列（ゼロ）が正しくファイルに書き込まれるかテスト
+    public function testStringZeroWriteToLog()
+    {
+        $data = "0";
+        Logger::dumpLog($this->testFilePath, $data);
+        $this->assertFileExists($this->testFilePath);
+        $this->assertEquals("0\r\n", file_get_contents($this->testFilePath));
+    }
+
     // 数値が正しくファイルに書き込まれるかテスト
     public function testIntegerWriteToLog()
     {
@@ -43,6 +69,42 @@ class LoggerTest extends TestCase
         Logger::dumpLog($this->testFilePath, $data);
         $this->assertFileExists($this->testFilePath);
         $this->assertEquals("100\r\n", file_get_contents($this->testFilePath));
+    }
+
+    // 数値（ゼロ）が正しくファイルに書き込まれるかテスト
+    public function testIntegerZeroWriteToLog()
+    {
+        $data = 0;
+        Logger::dumpLog($this->testFilePath, $data);
+        $this->assertFileExists($this->testFilePath);
+        $this->assertEquals("0\r\n", file_get_contents($this->testFilePath));
+    }
+
+    // ブール値（True）が正しくファイルに書き込まれるかテスト
+    public function testBoolTrueWriteToLog()
+    {
+        $data = true;
+        Logger::dumpLog($this->testFilePath, $data);
+        $this->assertFileExists($this->testFilePath);
+        $this->assertEquals("True\r\n", file_get_contents($this->testFilePath));
+    }
+
+    // ブール値（False）が正しくファイルに書き込まれるかテスト
+    public function testBoolFalseWriteToLog()
+    {
+        $data = false;
+        Logger::dumpLog($this->testFilePath, $data);
+        $this->assertFileExists($this->testFilePath);
+        $this->assertEquals("False\r\n", file_get_contents($this->testFilePath));
+    }
+
+    // null値が正しくファイルに書き込まれるかテスト
+    public function testNullWriteToLog()
+    {
+        $data = null;
+        Logger::dumpLog($this->testFilePath, $data);
+        $this->assertFileExists($this->testFilePath);
+        $this->assertEquals("Null\r\n", file_get_contents($this->testFilePath));
     }
 
     // 配列が正しくファイルに書き込まれるかテスト
